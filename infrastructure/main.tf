20c7a2407f35e4e21725d33b0a3196e35d124079
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2026-03-28 21:58:54 +0530

    pushing tummoc related infra and application code

---
+++ infrastructure/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "app_sg" {
+  name        = "app_security_group"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ec10929233384c7f"
+  instance_type = "t3.micro"
+  key_name = "tummoc"
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              EOF
+
+  tags = {
+    Name = "tummoc-assignment"
+  }
+}
