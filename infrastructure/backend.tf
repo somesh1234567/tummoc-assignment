80a9505cf2842b2f33bb30127dcdc558fe5e06d8
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2026-03-29 13:00:17 +0530

    changing s3 name

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "my-terraform-state-bucket-157"
+    bucket       = "my-terraform-state-bucket-158"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
