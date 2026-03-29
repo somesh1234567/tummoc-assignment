09c8415d8bc05ae7d279d6f1d3f861f37489ef6b
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2026-03-29 13:21:51 +0530

    added metrics endpoint

--- node-app/index.js
+++ node-app/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const client = require("prom-client");
 const app = express();
+
+//collect default metrics
+client.collectDefaultMetrics();
 
 app.get("/", (req, res) => {
   res.send("Hello from DevOps Pipeline!!!!");
+});
+
+// metrics endpoint
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
 });
 
 app.listen(3000, () => {
